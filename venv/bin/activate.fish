c73f51030e6ffbc3763d82845ad3148ebbbdd08d
Murbanski58 <114615052+Murbanski58@users.noreply.github.com>
2023-03-22 12:54:50 -0500

    created mapping and wrote questions

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ashlynyoung/Documents/GitHub/test2/venv"
+set -gx VIRTUAL_ENV "/Users/maggieurbanski/Documents/GitHub/test2/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH

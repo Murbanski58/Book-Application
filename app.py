5c236a8af27d5fea5b69ba017078c6281be932aa
Murbanski58 <114615052+Murbanski58@users.noreply.github.com>
2023-03-30 12:08:47 -0500

    flash

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, flash 
 
 app = Flask(__name__)
+
+app.config.update(
+    SECRET_KEY = "bananas"
+)
 
 book_list = [
     {"title": "The Hobbit", "author": "J.R.R Tolkien", "pages": "295", "classification": "fiction", "details":"read, recommend", "acquisition": "library"}
 ]
 
+# Handling error 404 and displaying relevant web page
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template("404.html"), 404
+
+# Handling error 500 and displaying relevant web page
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template("500.html"), 500
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -43,6 +56,11 @@ def add():
         print(book_dict)
         book_list.append(book_dict)
         print(book_list)
+        <div class="alert alert-success" role="alert">
+        Record successfully added!
+        flash('Record successfully added.' category = "success")
+        </div>
+        \
         return redirect(url_for("index"))
     else:
         return redirect(url_for("index"))
